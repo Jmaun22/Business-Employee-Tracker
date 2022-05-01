4ecf3afc04d25529e83856d2a378529d1a6c2726
Jmaun22 <98067577+Jmaun22@users.noreply.github.com>
2022-04-30 21:15:31 -0400

    added db folder and started the schema for sql

---
+++ db/schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS business_db;
+CREATE DATABASE business_db;
+
+USE business_db;
+
+CREATE TABLE department (
+    id INT NOT NULL PRIMARY KEY,
+    naem VARCHAR(30) NOT NULL
+)
+
+CREATE TABLE role (
+    id INT NOT NULL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL
+)
+
+CREATE TABLE employee (
+    id INT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL
+
+
+)
+
+
+
+
+
