32c686a4737977470b063e6b5b12b22a0e88d52b
Jmaun22 <98067577+Jmaun22@users.noreply.github.com>
2022-05-03 15:16:10 -0400

    added update function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const db = require("./db/connection.js");
 const express = require('express');
+const { changeUser } = require('./db/connection.js');
 
 
 const PORT = process.env.PORT || 3001;
@@ -13,106 +14,107 @@ app.use(express.json());
 
 
 
-const mainMenu = () => { 
+const mainMenu = () => {
     inquirer
-    .prompt([
-        {
+        .prompt([
+            {
 
-        type: "list",
-        name: "choice",
-        message: "What would you like to do?",
-        choices: [
-            {
-                name: "View All Employees",
-                value: "VIEW_EMPLOYEES"
-            },
-            // View Employees
-              // View Departments
-            {
-                name: "View All Departments",
-                value: "VIEW_DEPARTMENTS"
-            },
-             // ViewRoles
-            {
-                name: "View Roles",
-                value: "VIEW_ROLES"
+                type: "list",
+                name: "choice",
+                message: "What would you like to do?",
+                choices: [
+                    {
+                        name: "View All Employees",
+                        value: "VIEW_EMPLOYEES"
+                    },
+                    // View Employees
+                    // View Departments
+                    {
+                        name: "View All Departments",
+                        value: "VIEW_DEPARTMENTS"
+                    },
+                    // ViewRoles
+                    {
+                        name: "View Roles",
+                        value: "VIEW_ROLES"
 
-            },
-        
-            // ADD Emplyees
-            {
-                name: "Add Employees",
-                value: "ADD_EMPLOYEES"
+                    },
 
-            },
-              // ADD Department
-            {
-                name: "Add Department",
-                value: "ADD_DEPARTMENT"
-            },
-                 // Update and employee role
+                    // ADD Emplyees
+                    {
+                        name: "Add Employees",
+                        value: "ADD_EMPLOYEES"
 
-            {
-                name: "Update and employee role",
-                value: "UPDATE_ROLE"
+                    },
+                    // ADD Department
+                    {
+                        name: "Add Department",
+                        value: "ADD_DEPARTMENT"
+                    },
+                    // Update and employee role
 
-            },
-          
-       
-            {
-                name: "Quit",
-                value: "QUIT"
+                    {
+                        name: "Update and employee role",
+                        value: "UPDATE_ROLE"
+
+                    },
+
+
+                    {
+                        name: "Quit",
+                        value: "QUIT"
+                    }
+                ]
             }
-        ]
-    }
 
 
-    
 
-    ]).then(res => {
-        // now we call the appropriate function depending on what the user chooses
-        // if or switch
-        switch (res.choice) {
-            case "VIEW_EMPLOYEES":
-                console.log('switched')
-                viewEmployees();
-               
-                
-                break;
-            case "VIEW_DEPARTMENTS":
-                ViewDepartments();
 
-         
-                
-                 break;
-            case "VIEW_ROLES":
+        ]).then(res => {
+            // now we call the appropriate function depending on what the user chooses
+            // if or switch
+            switch (res.choice) {
+                case "VIEW_EMPLOYEES":
+                    console.log('switched')
+                    viewEmployees();
 
-                ViewRoles();
-                
-                break;
-            case "ADD_EMPLOYEES":
-                addEmployee();
-                
-                break;
-            case "ADD_DEPARTMENT":
-                addDepartment();
-                
-                break;
-            case "UPDATE_ROLE":
-                
-                 break;
-            case "QUIT":
-                quit();
-                
-                 break;
-        
-            default:
-                console.log("yo")
-                console.log(res.choice)
-                break;
-        }
 
-    })
+                    break;
+                case "VIEW_DEPARTMENTS":
+                    ViewDepartments();
+
+
+
+                    break;
+                case "VIEW_ROLES":
+
+                    ViewRoles();
+
+                    break;
+                case "ADD_EMPLOYEES":
+                    addEmployee();
+
+                    break;
+                case "ADD_DEPARTMENT":
+                    addDepartment();
+
+                    break;
+                case "UPDATE_ROLE":
+                    updateRole();
+
+                    break;
+                case "QUIT":
+                    quit();
+
+                    break;
+
+                default:
+                    console.log("yo")
+                    console.log(res.choice)
+                    break;
+            }
+
+        })
 
 }
 
@@ -124,12 +126,12 @@ mainMenu();
 
 function viewEmployees() {
     const sql = `SELECT * FROM employee`;
-    
+
     db.query(sql, function (err, results) {
         // console.table(results);
         console.table(results)
         mainMenu();
-      
+
     });
 
 };
@@ -172,52 +174,52 @@ function quit() {
 
 const addEmployee = () => {
     inquirer
-    .prompt([
- 
-        {
-            type: "input",
-            name: "employeeID",
-            message: "What is the employee id?",
+        .prompt([
 
-        },
+            {
+                type: "input",
+                name: "employeeID",
+                message: "What is the employee id?",
 
-        {
-            type: "input",
-            name: "employeeFirstName",
-            message: "What is your employees firstname?",
+            },
 
-        },
-        {
-            type: "input",
-            name: "emplyoeeLastName",
-            message: "What is your employees lastname?",
+            {
+                type: "input",
+                name: "employeeFirstName",
+                message: "What is your employees firstname?",
 
-        },
-        {
-            type: "input",
-            name: "employeeRoleId",
-            message: "What is your employees roleId?",
+            },
+            {
+                type: "input",
+                name: "emplyoeeLastName",
+                message: "What is your employees lastname?",
 
-        },
-        {
-            type: "input",
-            name: "employeeManagerId",
-            message: "What is your employees manager_id",
+            },
+            {
+                type: "input",
+                name: "employeeRoleId",
+                message: "What is your employees roleId?",
 
-        }
-    ]).then((results) => {
-        console.log(results);
-        console.log(typeof results.employeeID, typeof results.employeeFirstName, typeof results.emplyoeeLastName, typeof results.employeeRoleId, typeof results.employeeManagerId);
-        const sql = `INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+            },
+            {
+                type: "input",
+                name: "employeeManagerId",
+                message: "What is your employees manager_id",
+
+            }
+        ]).then((results) => {
+            console.log(results);
+            console.log(typeof results.employeeID, typeof results.employeeFirstName, typeof results.emplyoeeLastName, typeof results.employeeRoleId, typeof results.employeeManagerId);
+            const sql = `INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
         VALUES (?, ?, ?, ?, ?)`;
-      const params = [Number(results.employeeID), results.employeeFirstName, results.emplyoeeLastName, Number(results.employeeRoleId), Number(results.employeeManagerId)];
-      
-      db.query(sql, params, (err, result) => {
+            const params = [Number(results.employeeID), results.employeeFirstName, results.emplyoeeLastName, Number(results.employeeRoleId), Number(results.employeeManagerId)];
 
-          console.log(results);
-          mainMenu();
-        })
-    });
+            db.query(sql, params, (err, result) => {
+
+                console.log(results);
+                mainMenu();
+            })
+        });
 }
 
 // 
@@ -229,36 +231,123 @@ const addEmployee = () => {
 
 const addDepartment = () => {
     inquirer
-    .prompt([
- 
-        {
-            type: "input",
-            name: "departmentID",
-            message: "What is the department id?",
+        .prompt([
 
-        },
+            {
+                type: "input",
+                name: "departmentID",
+                message: "What is the department id?",
 
-        {
-            type: "input",
-            name: "departmentName",
-            message: "What is the name of your department",
+            },
 
-        }
-       
-    ]).then((results) => {
-        console.log(results);
-        console.log(results.departmentID, results.departmentName);
-        const sql = `INSERT INTO department (id, name)
+            {
+                type: "input",
+                name: "departmentName",
+                message: "What is the name of your department",
+
+            }
+
+        ]).then((results) => {
+            console.log(results);
+            console.log(results.departmentID, results.departmentName);
+            const sql = `INSERT INTO department (id, name)
         VALUES (?, ?)`;
-      const params = [Number(results.departmentID), results.departmentName];
-      
-      db.query(sql, params, (err, result) => {
+            const params = [Number(results.departmentID), results.departmentName];
 
-          console.log(results);
-          mainMenu();
-        })
-    });
+            db.query(sql, params, (err, result) => {
+
+                console.log(results);
+                mainMenu();
+            })
+        });
 }
+
+// update roles 
+
+const updateRole = () => {
+    const sql = `SELECT first_name, id  FROM employee`;
+
+
+    db.query(sql, function (err, results) {
+
+        // console.table(results)
+        // console.log(results);
+
+
+        var names = results.map(user => {
+            return {
+                name: user.first_name,
+                value: user.id
+            }
+
+
+        });
+        console.log(names)
+
+
+        inquirer
+            .prompt([{
+                type: "list",
+                name: "employeeId",
+                message: "Which employee would you like to update their role?",
+                choices: names
+
+            },
+
+            ]).then((answer) => {
+
+                var employeeID = answer.employeeId
+
+                const sql = `SELECT id, title FROM role`;
+                // const sql = `UPDATE  SET review = ? WHERE id = ?`;
+                const params = [];
+
+                db.query(sql, params, (err, roles) => {
+
+                    // console.log(roles);
+                    // console.table(roles);
+
+                    var allRoles = roles.map(role => {
+                        return {
+                            name: role.title,
+                            value: role.id
+                        }
+                    });
+                    inquirer
+                        .prompt([{
+                            type: "list",
+                            name: "rolesId",
+                            message: "What role would you like to give them?",
+                            choices: allRoles
+
+                        },
+
+                        ]).then((roleAnswer) => {
+
+                            var roleID = roleAnswer.rolesId
+
+                            // console.log(employeeID);
+                            // console.log(roleAnswer);
+
+
+
+                            const sql = `UPDATE  employee SET role_Id = ${roleID} WHERE id = ${employeeID}`;
+                            const params = [];
+
+                            db.query(sql, params, (err, res) => {
+
+                                console.log('Updated employees role')
+
+                                mainMenu();
+                            });
+
+                        });
+                });
+
+            });
+    })
+}
+
 
 
 
